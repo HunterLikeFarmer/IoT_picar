8746f5ed517c52093e1c6987334d00a113e8da8d
Hunter Jiang <hunter789654@gmail.com>
2026-02-23 22:35:24 -0600

    add kick lonely point

--- lab1b_test/scan.py
+++ lab1b_test/scan.py
@@ -28,6 +28,7 @@ def scan_environment(px, current_pos, current_heading, visited_pos, visited_bloc
                 grid[y_val, x_val] = 1
             
     #connect_point(grid)
+    grid = kick_lonely_point(grid)
     # adding to visited_block
     for i in range(GRID_HEIGHT):
         for j in range(GRID_WIDTH):
@@ -45,6 +46,30 @@ def scan_environment(px, current_pos, current_heading, visited_pos, visited_bloc
         grid[vy, vx] = 1
         
     return grid
+
+# return a list of all lonely point
+def kick_lonely_point(grid):
+    to_change = []
+    for i in range(GRID_HEIGHT):
+        for j in range(GRID_WIDTH):
+            count_one = 0
+            c_arr = [-1, 0, 1]
+            r_arr = [-1, 0, 1]
+            for c in c_arr:
+                for r in r_arr:
+                    if c == 0 and r == 0:
+                        continue
+                    cur_c = j + c
+                    cur_r = i + r
+                    if cur_c >= 0 and cur_r >= 0 and cur_r < GRID_HEIGHT and cur_c < GRID_WIDTH and grid[cur_r, cur_c] == 1:
+                        count_one += 1
+            if count_one == 0:
+                to_change.append((i, j))
+    for tup in to_change:
+        r, c = tup
+        grid[r, c] = 1
+    return grid
+    
 
 def connect_point(grid):
     to_change = []
